2da81e5824a32cc12530d3163b39fb258d556598
Ed <ea.litten@gmail.com>
2018-02-28 12:18:05 +0000

    DRY changing quality and sell_in into separate methods

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -11,48 +11,60 @@ class GildedRose
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            decrease_quality(item)
           end
         end
       else
         if item.quality < 50
-          item.quality = item.quality + 1
+          increase_quality(item)
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                increase_quality(item)
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                increase_quality(item)
               end
             end
           end
         end
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+        decrease_sell_in(item)
       end
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                decrease_quality(item)
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality = 0
           end
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            increase_quality(item)
           end
         end
       end
     end
   end
+end
+
+def increase_quality(item)
+  item.quality += 1 
+end
+
+def decrease_quality(item)
+  item.quality -= 1 
+end
+
+def decrease_sell_in(item)
+  item.sell_in -= 1
 end
 
 class Item
