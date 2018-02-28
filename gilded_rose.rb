71a8ae268e4507716fb195b72cfa4c402a628488
Ed <ea.litten@gmail.com>
2018-02-28 13:53:02 +0000

    Refactor +/- quality methods to take increment, refactor main case block accordingly

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -8,35 +8,40 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-
       case item.name
       when "Sulfuras, Hand of Ragnaros"
         next
       when "Aged Brie"
-        increase_quality(item)
-        increase_quality(item) if item.sell_in <= 0
+        if item.sell_in > 0
+          increase_quality(item, 1)
+        else
+          increase_quality(item, 2)
+        end
       when "Backstage passes to a TAFKAL80ETC concert"
-        increase_quality(item)
-        increase_quality(item) if item.sell_in < 11
-        increase_quality(item) if item.sell_in < 6
-        item.quality = 0 if item.sell_in <= 0
+        case item.sell_in
+        when 11..Float::INFINITY then increase_quality(item, 1)
+        when 6..10 then increase_quality(item, 2)
+        when 1..5 then increase_quality(item, 3)
+        else item.quality = 0
+        end
       else
-        decrease_quality(item)
-        decrease_quality(item) if item.sell_in <= 0
+        if item.sell_in > 0
+          decrease_quality(item, 1)
+        else
+          decrease_quality(item, 2)
+        end
       end
-
       decrease_sell_in(item)
-
     end
   end
 end
 
-def increase_quality(item)
-  item.quality += 1 if item.quality < 50
+def increase_quality(item, increment = 1)
+  increment.times { item.quality += 1 if item.quality < 50 }
 end
 
-def decrease_quality(item)
-  item.quality -= 1 if item.quality > 0
+def decrease_quality(item, increment = 1)
+  increment.times { item.quality -= 1 if item.quality > 0 }
 end
 
 def decrease_sell_in(item)
