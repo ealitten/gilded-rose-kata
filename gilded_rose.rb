fa9bef0a63f58e6712be2591c8ffce3d3ad03cfb
Ed <ea.litten@gmail.com>
2018-02-28 13:11:51 +0000

    Refactor first major if block into case statement

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -8,21 +8,22 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      next if item.name == "Sulfuras, Hand of Ragnaros"
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        decrease_quality(item)
-      else
+
+      case item.name
+      when "Sulfuras, Hand of Ragnaros"
+        next
+      when "Aged Brie"
         increase_quality(item)
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in < 11
-              increase_quality(item)
-          end
-          if item.sell_in < 6
-              increase_quality(item)
-          end
-        end
+      when "Backstage passes to a TAFKAL80ETC concert"
+        increase_quality(item)
+        increase_quality(item) if item.sell_in < 11
+        increase_quality(item) if item.sell_in < 6
+      else
+        decrease_quality(item)
       end
+
       decrease_sell_in(item)
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -36,6 +37,7 @@ class GildedRose
       end
     end
   end
+
 end
 
 def increase_quality(item)
